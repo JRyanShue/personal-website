9c2eef3389d4b34f6b14bd6ac6aa8d80fd75cc5d
JRyanShue <ryan_shue23@milton.edu>
2022-01-30 15:40:12 -0500

    Update FrontMenubar.js

--- src/FrontMenubar.js
+++ src/FrontMenubar.js
@@ -10,7 +10,7 @@ function FrontMenubar() {
             </div>
             <div 
             id="resume"
-            onClick={() => {window.open('https://docs.google.com/document/d/12di_u-VAEeL1y3q44KheB3eU25YZYMiRr9ws9ffI_xQ/edit')}}
+            onClick={() => {window.open('https://s3.us-east-1.amazonaws.com/jryanshue.com/Resume.pdf?')}}
             onMouseHover={() => {document.getElementById('resume').style.color = '#ff0000'}}
             style={{cursor:'pointer', marginLeft:'10px', marginRight:'10px'}}
             >
