cfdd52229ce17b743d08b77301e2c60cdb1a2983
JRyanShue <ryan_shue23@milton.edu>
2022-03-14 20:06:46 -0400

    Update FrontLinks.js

--- src/FrontLinks.js
+++ src/FrontLinks.js
@@ -5,7 +5,7 @@ function FrontLinks() {
     return (
         <div className='front-links'>
             <SocialIcon url='https://github.com/JRyanShue' style={{width:'40px', height:'40px', margin:'5px'}}/>
-            <SocialIcon url='https://www.linkedin.com/in/ryan-shue-27656120b/' style={{width:'40px', height:'40px', margin:'5px'}}/>
+            <SocialIcon url='https://www.linkedin.com/in/j-ryan-shue-27656120b/' style={{width:'40px', height:'40px', margin:'5px'}}/>
             <SocialIcon url='mailto:ryan_shue23@milton.edu' style={{width:'40px', height:'40px', margin:'5px'}}/>
         </div>
     )
