bfe5b060eb25faebc93a05d10942464df6df77a0
JRyanShue <ryan_shue23@milton.edu>
2022-03-22 01:49:02 +0100

    Update FrontAbout.js

--- src/FrontAbout.js
+++ src/FrontAbout.js
@@ -24,14 +24,14 @@ function FrontAbout() {
             <div className="front-about" style={{width: textWidth}}>
                 <i class="fab fa-github"></i>
                 I'm a high school junior at Milton Academy, where I'm a co-head of our Robotics Team and a News Editor for our school paper. 
-                I'm currently working with collaborators on an AI research project applying Transformers to generating Mesh Implicit Neural Representations. 
+                I'm currently working with collaborators on an AI research project applying Transformers to single-shot 3D Neural Field Generation. 
                 <br/><br/>
                 I'm passionate about building software and hardware that matters. 
                 In the past, I've worked on a novel 3D printer for automated part production (called "<a href="https://jryanshue.github.io/ZengerWriterV1/" style={{color:'rgb(214, 214, 214)'}}>The Zenger</a>") 
-                and co-founded the creation of a stackable hydroponic system (called "<a href="https://www.kickstarter.com/projects/thecanopy/the-canopy-smart-vertical-farm" style={{color:'rgb(214, 214, 214)'}}>The Canopy</a>"), which we successfully funded on Kickstarter. I've previously also had the opportunity to work at <></>
+                and co-founded the creation of a stackable hydroponic system (called "<a href="https://www.kickstarter.com/projects/thecanopy/the-canopy-smart-vertical-farm" style={{color:'rgb(214, 214, 214)'}}>The Canopy</a>"), which we successfully funded on Kickstarter and are currently working on producing. I've previously also had the opportunity to work at <></>
                 <a href="https://ubiik.com" style={{color:'rgb(214, 214, 214)'}}>Ubiik Inc.</a>, first as an intern and later as an Industrial Engineer.  
                 <br/><br/>
-                I'm pretty well-versed in the literatures of Supervised and Unsupervised Learning. I have experience with Python/Pytorch/TensorFlow, Solidworks CAD, full-stack web development, and FDM 3D printing. 
+                I'm pretty well-versed in the literatures of Supervised and Unsupervised Learning. I have experience with Python/PyTorch/TensorFlow, Solidworks CAD, full-stack web development, and FDM 3D printing. 
                 <br/><br/>
                 Feel free to reach out to me for anything! My email is jryanshue [at] gmail [dot] com.
                 <br/>
