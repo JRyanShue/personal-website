99f62235f7c3be2c8eae2d9e8a40ff9871ae04df
JRyanShue <ryan_shue23@milton.edu>
2022-03-26 22:04:05 +0000

    Update FrontAbout.js

--- src/FrontAbout.js
+++ src/FrontAbout.js
@@ -24,12 +24,12 @@ function FrontAbout() {
             <div className="front-about" style={{width: textWidth}}>
                 <i class="fab fa-github"></i>
                 I'm a high school junior at Milton Academy, where I'm a co-head of our Robotics Team and a News Editor for our school paper. 
-                I'm currently working with collaborators on an AI research project applying transformers to single-shot 3D neural field generation. The goal is to build a model that will map a 3D shape into a neural field parameterizing it. Neural fields already have many applications, but I'm most excited about the promise they hold as more perfect representations of data, and what this means for AI as a whole. 
+                I'm currently working with collaborators on an AI research project applying vanilla transformers (ViT style) to single-shot 3D neural field generation. Neural fields already have many applications, but I'm most excited about the promise they hold as more perfect representations of data, and what this means for AI as a whole. 
                 <br/><br/>
                 I'm passionate about building software and hardware that matters. 
-                In the past, I've worked on a novel 3D printer for automated part production (called "<a href="https://jryanshue.github.io/ZengerWriterV1/" style={{color:'rgb(214, 214, 214)'}}>The Zenger</a>") 
-                and co-founded the creation of a stackable hydroponic system (called "<a href="https://www.kickstarter.com/projects/thecanopy/the-canopy-smart-vertical-farm" style={{color:'rgb(214, 214, 214)'}}>The Canopy</a>"), which we successfully funded on Kickstarter and are currently working on producing. I've previously also had the opportunity to work at <></>
-                <a href="https://ubiik.com" style={{color:'rgb(214, 214, 214)'}}>Ubiik Inc.</a>, first as an intern and later as an Industrial Engineer.  
+                In the past, I've worked on a novel 3D printer (and its corresponding software) for automated part production (called "<a href="https://jryanshue.github.io/ZengerWriterV1/" style={{color:'rgb(214, 214, 214)'}}>The Zenger</a>"), with the purpose of making 3D printing viable for economic small-scale production. 
+                I also co-founded the creation of a stackable hydroponic system (called "<a href="https://www.kickstarter.com/projects/thecanopy/the-canopy-smart-vertical-farm" style={{color:'rgb(214, 214, 214)'}}>The Canopy</a>"), aimed at allowing consumers to grow their own food in small indoor spaces. The project was funded on Kickstarter last year, and we've been working on production. I've previously also had the opportunity to work at <></>
+                <a href="https://ubiik.com" style={{color:'rgb(214, 214, 214)'}}>Ubiik Inc.</a>, designing and prototyping parts first as an intern and later as a contract-based Industrial Engineer.  
                 <br/><br/>
                 I'm pretty well-versed in the deep learning literatures of Neural Fields, Natural Language Processing, and Computer Vision. I have experience with Python and PyTorch/TensorFlow, Solidworks CAD and Industrial/Mechanical Design, full-stack web development, and FDM 3D printing. 
                 <br/><br/>
